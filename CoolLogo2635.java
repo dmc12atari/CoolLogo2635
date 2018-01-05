a9ca8751586f52a833b6af8cc971c137aa9e2be2
dmc12atari <32552321+dmc12atari@users.noreply.github.com>
2018-01-05 11:28:29 -0800

    advanced info and jar file

--- CoolLogo2635.java
+++ CoolLogo2635.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 public class CoolLogo2635 {
     public static void main(String args[]) {
---
+++ main.java
@@ -0,0 +1,7 @@
+
+
+public class main extends CoolLogo2635 {
+    public static void main(String args[]) {
+        recurse();
+    }
+}
